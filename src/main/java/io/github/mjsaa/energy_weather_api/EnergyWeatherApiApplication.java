858eddd828b36b8853cb0d2ab028cd07bcb5a123
Mustafa Al-Abaychi <mustafa.al-abaychi@bouvet.se>
2025-11-03 09:18:18 +0100

    Initial commit

---
+++ src/main/java/io/github/mjsaa/energy_weather_api/EnergyWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.mjsaa.energy_weather_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyWeatherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mjsaa/energy_weather_api/EnergyWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mjsaa.energy_weather_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
